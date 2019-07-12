ce315d3d112954a42c5bdbcc1832920ee400bfe1
Matt Cooper <28823876+Matt-dc@users.noreply.github.com>
2019-07-12 20:01:58 +0200

    Add files via upload

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,20 +1,29 @@
-from tweepy import API
-from tweepy import Cursor
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from textblob import TextBlob
+
+from tweepy import API
+from tweepy import Cursor
 
 import twitter_credentials
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import tkinter as Tk
+import matplotlib.pyplot as plt, mpld3
+
+import datetime as dt
+
 import re
+from textblob import TextBlob
+
+import spacy
+from collections import Counter
+nlp = spacy.load('en')
+
+from wtforms import Form, StringField, validators
 
 
 
-#Twitter Client
 class TwitterClient():
 
     def __init__(self, twitter_user=None):
@@ -24,8 +33,7 @@ class TwitterClient():
         self.twitter_user = twitter_user
 
     def get_twitter_client_api(self):
-        return self.twitter_client
-
+         return self.twitter_client
 
     def get_user_timeline_tweets(self, num_tweets):
         tweets = []
@@ -35,19 +43,18 @@ class TwitterClient():
 
     def get_friend_list(self, num_friends):
         friend_list = []
-        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
-            friend_list.append(friend)
-        return friend_list
+        for friend in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_friends):
+            num_friends.append(friend)
+        return num_friends
 
     def get_home_timeline_tweets(self, num_tweets):
-        home_timeline_tweets = []
-        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+        home_timeline_tweets =[]
+        for tweet in Cursor(self.twitter_client.home_timeline).items(num_tweets):
             home_timeline_tweets.append(tweet)
         return home_timeline_tweets
 
 
 
-#Twitter authenticator
 class TwitterAuthenticator():
 
     def authenticate_twitter_app(self):
@@ -56,35 +63,25 @@ class TwitterAuthenticator():
         return auth
 
 
-#Authentication: class for streaming and processing live tweets
-class TwitterStreamer():
+
+class Streamer():
 
     def __init__(self):
         self.twitter_authenticator = TwitterAuthenticator()
 
-    def stream_tweets(self, fetched_tweets_filename, hashtag_list):
-        #Handles twitter authentificationa nd connectio to streaming API
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
         listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter_app()
 
-        # pass in auth token and listener object - which determines tweet data and error handling
-        auth = self.twitter_autenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
-
-        # use filter method to filter tweets
-        # track arg is a list which allows us to set our filter
         stream.filter(track=hash_tag_list)
 
 
 
-#on_data and on_error are twitter classes that can be overridden in our code
-#on_data takes in data from the stream listener
-#on_error if there is an error in streaming
-#basic listener class that prints tweets to stdout
 class TwitterListener(StreamListener):
 
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
-
 
     def on_data(self, data):
         try:
@@ -96,23 +93,19 @@ class TwitterListener(StreamListener):
             print("Error on data: %s" % str(e))
         return True
 
-
-    def on_error(self,status):
+    def on_error(self, status):
         if status == 420:
             return False
-            #returning false on data method in case rate limit is surpassed
         print(status)
 
 
 
-#analyse and categorise tweet content
 class TweetAnalyzer():
-    """
-    Functionality for analyzing and categorizing content from tweets.
-    """
+
 
     def clean_tweet(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
 
     def analyze_sentiment(self, tweet):
         analysis = TextBlob(self.clean_tweet(tweet))
@@ -124,49 +117,87 @@ class TweetAnalyzer():
         else:
             return -1
 
+    def find_most_common_words(self, tweet_string):
+        parsed_tweet_string = nlp(tweet_string)
+        cleaned = [ token.text for token in parsed_tweet_string if token.is_stop != True and token.is_punct != True and token.pos_ == ('NOUN' or 'VERB' or 'PROPN') ]
+        word_count = Counter(cleaned)
+        most_common_words = word_count.most_common(15)
+        return most_common_words 
+        
 
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
 
-        df['id'] = np.array([tweet.id for tweet in tweets])
-        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        # df['id'] = np.array([tweet.id for tweet in tweets])
+        # df['len'] = np.array([len(tweet.text) for tweet in tweets])
         df['date'] = np.array([tweet.created_at for tweet in tweets])
-        df['source'] = np.array([tweet.source for tweet in tweets])
+        # df['source'] = np.array([tweet.source for tweet in tweets])
         df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
-        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])      
+        df['sentiment'] = np.array([self.analyze_sentiment(tweet) for tweet in df['tweets']])
 
         return df
+      
+ 
+    def get_daily_total(self, df):
+        count_df = df['date'].groupby(df['date'].dt.floor('d')).size().reset_index(name='count') 
+        daily_totals = count_df['count']
+        return daily_totals
 
 
-if __name__ == '__main__':
-    twitter_client = TwitterClient()
-    tweet_analyzer = TweetAnalyzer()
+class Tweeter():
 
-    api = twitter_client.get_twitter_client_api()
-
-    tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
-
-    df = tweet_analyzer.tweets_to_data_frame(tweets)
-    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
-
-    print(df.head(10))
+    def __init__(self):
+        self.twitter_client = TwitterClient()
+        self.tweet_analyzer = TweetAnalyzer()
+        self.api = TwitterClient().get_twitter_client_api()
 
 
+    ## staying DRY
+    def get_tweets(self, user, count):
+        tweets = self.api.user_timeline(screen_name=user, count=count)
+        return tweets
 
 
-    #get average tweet length
+    def latest_tweets(self, user, count):
+        tweets = self.get_tweets(user, count)
+        df = self.tweet_analyzer.tweets_to_data_frame(tweets)
+        latest = df[['tweets', 'date']].head(5)
+        return latest
 
-    #get average tweet length
-#    print(np.mean(df['len']))
 
-    #get likes of most-liked tweet
- #   print(np.max(df['likes']))
+    def most_popular(self, user, count):
+        tweets = self.get_tweets(user, count)
+        df = self.tweet_analyzer.tweets_to_data_frame(tweets)
+        most_popular = df.sort_values(by=['likes'], ascending=False)
+        popular_selection = most_popular.head(5)
+        return popular_selection
 
-    #get no. of retweets of most retweeted tweet
-  #  print(np.max(df['retweets']))
 
-    #Time series
- #   time_likes = pd.Series(data=df['likes'].values, index=df['date'])
- #   time_likes.plot(figsize=(16, 4), color='r')
-#    plt.show()
+    def positivity_rating(self, user, count):
+        tweets = self.get_tweets(user, count)
+        df = self.tweet_analyzer.tweets_to_data_frame(tweets)
+        positivity = df['sentiment']
+        overall_positivity = np.mean(positivity)
+        return overall_positivity
+
+
+    def posting_frequency(self, user, count):
+        tweets = self.get_tweets(user, count)
+        df = self.tweet_analyzer.tweets_to_data_frame(tweets)
+        daily_totals = self.tweet_analyzer.get_daily_total(df)
+        fig = plt.figure()
+        plt.plot(daily_totals)
+        plt.xlabel('Number of daily tweets')
+        htmlfig = mpld3.fig_to_html(fig)
+        return htmlfig
+    
+
+    def most_common_words(self, user, count):       
+        tweets = self.get_tweets(user, count)
+        tweet_array = np.array([ tweet.text for tweet in tweets ])
+        tweet_bag = np.array2string(tweet_array)    
+        most_common_words = self.tweet_analyzer.find_most_common_words(tweet_bag)
+        return most_common_words
+
 
